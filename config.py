c7cebdc5f5401d2e7e3983760d5806428f2000c7
Christophe Simonis <christophe@kn.gl>
2013-01-05 19:37:20 +0100

    Follow the specifications [1][2] and allow service secret keys to be empty
    [1] http://tools.ietf.org/html/rfc5849#section-3.4.4
    [2] http://tools.ietf.org/html/draft-ietf-oauth-v2-31#section-2.3.1

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ for module_name in get_service_modules():
         key = os.environ.get('%s_KEY' % alias, '').decode('utf8')
         secret = os.environ.get('%s_SECRET' % alias, '').decode('utf8')
 
-        if key and secret:  # Only initialize if all the pieces are in place
+        if key:  # Only initialize if all the pieces are in place
             services.append(service(key, secret))
 
 
